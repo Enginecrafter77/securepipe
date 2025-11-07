0504f186ab292da9359b891654f48ecd10e667b8
Enginecrafter77 <hutiramichal@gmail.com>
2025-11-07 19:43:11 +0100

    feat: TCP socket transport

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::{fs::File, io::{self, Read, Write}};
+use std::{fs::File, io::{self, Read, Write}, net::{IpAddr, Ipv4Addr, SocketAddr, SocketAddrV4, TcpListener, TcpStream}};
 
 use aes_gcm::{AeadCore, Aes256Gcm, KeyInit, Nonce, aead::{AeadMutInPlace, consts::U12, rand_core::SeedableRng}};
+use dns_lookup::lookup_host;
 use getopts::Options;
 
 use crate::{rng::StdRngWrapper};
@@ -24,7 +25,7 @@ impl CryptPipeContext {
         return Aes256Gcm::generate_nonce(&mut self.rng);
     }
 
-    fn encrypt_round(&mut self, src: &mut Box<dyn Read>, dst: &mut Box<dyn Write>) -> io::Result<usize> {
+    fn encrypt_round(&mut self, src: &mut Box<dyn Read>, dst: &mut Box<TcpStream>) -> io::Result<usize> {
         let nonce = self.new_nonce();
 
         self.buffer.resize(self.default_buffer_size, 0);
@@ -43,7 +44,7 @@ impl CryptPipeContext {
         return Ok(self.buffer.len());
     }
 
-    fn decrypt_round(&mut self, src: &mut Box<dyn Read>, dst: &mut Box<dyn Write>) -> io::Result<usize> {
+    fn decrypt_round(&mut self, src: &mut Box<TcpStream>, dst: &mut Box<dyn Write>) -> io::Result<usize> {
         let nonce = self.new_nonce();
 
         src.read_exact(&mut self.len_buffer)?;
@@ -75,6 +76,28 @@ macro_rules! extract_array {
     };
 }
 
+fn resolve_name(name: &str) -> Result<IpAddr, io::Error> {
+    let Some(addr) = lookup_host(name)?.find(|a| a.is_ipv4()) else {
+        return Err(io::Error::new(io::ErrorKind::AddrNotAvailable, "No address available"));
+    };
+    return Ok(addr);
+}
+
+fn obtain_socket(host: Option<String>, port: u16) -> Result<(TcpStream, SocketAddr), io::Error> {
+    match host {
+        None => { // server
+            let serversocket = TcpListener::bind(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, port))?;
+            return serversocket.accept();
+        },
+        Some(hostname) => {
+            let addr = resolve_name(hostname.as_str())?;
+            let sockaddr = SocketAddr::new(addr, port);
+            let socket = TcpStream::connect(sockaddr)?;
+            return Ok((socket, sockaddr));
+        }
+    }
+}
+
 fn main() {
     let key_v = hex::decode(std::env::var("SP_KEY").expect("SP_KEY env variable not set")).expect("Illegal value in SP_KEY");
     let seed_v = hex::decode(std::env::var("SP_SEED").expect("SP_SEED env variable not set")).expect("Illegal value in SP_SEED");
@@ -86,6 +109,7 @@ fn main() {
     opts.optflag("d", "decrypt", "Designated decrypting end");
     opts.optopt("i", "input", "Read input from", "source");
     opts.optopt("o", "output", "Write output to", "dest");
+    opts.optopt("p", "port", "Use the given port", "port");
     opts.optopt("b", "block-size", "The size of plaintext blocks", "block-size");
 
     let m = opts.parse(std::env::args()).expect("Parsing arguments failed");
@@ -94,6 +118,14 @@ fn main() {
         writeln!(io::stderr(), "{}\n{}", opts.short_usage("securepipe"), opts.usage("A simple pipe for secure network transfers")).expect("Writing help failed");
         return;
     }
+
+    let port = m.opt_str("p").map(|x| x.parse::<u16>().expect("Parsing port number failed")).unwrap_or(4096);
+    let Ok((socket, _)) = obtain_socket(m.free.get(1).cloned(), port) else {
+        writeln!(io::stderr(), "Unable to estabilish connection").expect("STDERR write failed");
+        return;
+    };
+
+    let mut boxed_socket = Box::new(socket);
 
     let mut src: Box<dyn Read> = match m.opt_str("i") {
         None => Box::new(io::stdin()),
@@ -109,9 +141,9 @@ fn main() {
     let mut blen: usize = 1;
     while blen > 0 {
         if m.opt_present("d") {
-            blen = ctx.decrypt_round(&mut src, &mut dest).expect("IO error");
+            blen = ctx.decrypt_round(&mut boxed_socket, &mut dest).expect("IO error");
         } else {
-            blen = ctx.encrypt_round(&mut src, &mut dest).expect("IO error");
+            blen = ctx.encrypt_round(&mut src, &mut boxed_socket).expect("IO error");
         }
     }
 }
